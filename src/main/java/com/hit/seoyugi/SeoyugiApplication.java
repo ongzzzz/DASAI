6eea509f037434738eef80439f539b8ceeb1ae7a
jaeseonNamgung <sunnamgung8@naver.com>
2024-03-18 14:23:40 +0800

    Initial commit

---
+++ src/main/java/com/hit/seoyugi/SeoyugiApplication.java
@@ -0,0 +1,13 @@
+package com.hit.seoyugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoyugiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoyugiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hit/seoyugi/SeoyugiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hit.seoyugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoyugiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
